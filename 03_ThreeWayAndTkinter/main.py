0e0d5990ed25ba3db1e63ab483dfb73182d61f79
Алина Калякина <alinakalakina@MacBook-Pro-Alina.local>
2021-03-13 19:22:33 +0300

    Добавлена отрисовка кнопок игры и функционал создания и завершения игры
    Добавлена отрисовка кнопок-цифр, а также кнопок "New" и "Exit". New cоздает игру, Exit завершает. Масштабирование происходит правильно

---
+++ 03_ThreeWayAndTkinter/main.py
@@ -0,0 +1,52 @@
+
+import random
+
+import tkinter as tk
+
+
+class Game15():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Пятнашки")
+        # сделаем, чтобы колоночки и ряды масштабровались одинаково
+        self.window.rowconfigure(0, weight=1, uniform="row_gr")
+        for i in range(4):
+            self.window.rowconfigure(i + 1, weight=1, uniform="row_gr")
+            self.window.columnconfigure(i, weight=1, uniform="col_gr")
+
+        self.new_button = tk.Button(self.window, text="New", command=self.new_click)
+        self.new_button.grid(column=0, row=0, columnspan=2)
+        self.exit_button = tk.Button(self.window, text="Exit", command=self.exit)
+        self.exit_button.grid(column=2, row=0, columnspan=2)
+        self.numbers = tuple((tk.Button(self.window, text=str(i+1), command=self.number_click(i)) for i in range(15)))
+        self.new_click()
+
+    def new_click(self):
+        self.buttons_order = list(range(15))
+        random.shuffle(self.buttons_order)
+        # пустота
+        self.buttons_order.append(None)
+        self.place_buttons()
+
+    def exit(self):
+        self.window.destroy()
+
+    def place_buttons(self):
+        for i, button_id in enumerate(self.buttons_order):
+            if button_id is None:
+                continue
+            self.numbers[button_id].grid(column=i % 4, row=1 + i // 4, sticky=tk.NSEW)
+
+    def number_click(self, n):
+        def move_fn():
+            pass
+        return move_fn
+
+
+    def run(self):
+        self.window.mainloop()
+
+
+if __name__ == '__main__':
+    game = Game15()
+    game.run()
